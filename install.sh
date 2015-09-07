033ddb981bd9f4b40a9e0439259acb7961b0bd33
Jags <jagan@Jagans-MacBook-Pro.local>
2015-09-07 19:48:44 +1000

    Checks if the file has execute permissions.

--- install.sh
+++ install.sh
@@ -6,11 +6,19 @@ GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases.sh
 SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases.sh
 BASH_PROFILE_PATH='/Users/'$USERNAME'/.bash_profile'
 
+if [ -e $GIT_ALIASES_PATH -a ! -x $GIT_ALIASES_PATH ]; then
+  chmod +x $GIT_ALIASES_PATH
+fi
+
+if [ -e $SYS_ALIASES_PATH -a ! -x $SYS_ALIASES_PATH ]; then
+  chmod +x $SYS_ALIASES_PATH
+fi
+
 if [ -e $BASH_PROFILE_PATH -a -e $GIT_ALIASES_PATH -a -e $SYS_ALIASES_PATH ]; then
   echo $GIT_ALIASES_PATH >> $BASH_PROFILE_PATH
   echo $SYS_ALIASES_PATH >> $BASH_PROFILE_PATH
   source $BASH_PROFILE_PATH
 else
-  echo '.bash_profile not found. PWD == > $DOTFILES_ROOT.'
+  echo 'One or more files not found. PWD => $DOTFILES_ROOT.'
   echo ' The "install.sh" has to be run from inside the "dotfiles" folder.'
 fi
