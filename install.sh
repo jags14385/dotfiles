45a543aea002d91ceb67bb1b4a43bf545567d167
Jags <jagan@Jagans-MacBook-Pro.local>
2015-09-07 19:37:35 +1000

    Checks for the existence of the files to be loaded.

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+USERNAME=$USER
 DOTFILES_ROOT=$(pwd)
-EXP_PATH=$DOTFILES_ROOT/exp.sh
+GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases.sh
+SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases.sh
+BASH_PROFILE_PATH='/Users/'$USERNAME'/.bash_profile'
 
-if [ -f '~/.bash_profile' ]; then
-  echo $PATH >> ~/.bash_profile
-  source $HOME/.bash_profile
-  cat $HOME/.bash_profile
+if [ -e $BASH_PROFILE_PATH -a -e $GIT_ALIASES_PATH -a -e $SYS_ALIASES_PATH ]; then
+  echo $GIT_ALIASES_PATH >> $BASH_PROFILE_PATH
+  echo $SYS_ALIASES_PATH >> $BASH_PROFILE_PATH
+  source $BASH_PROFILE_PATH
 else
-  echo 'n'
-  echo `source $PATH`
-fi
-
-if [ -f '$HOME/.bashrc' ]; then
-  source $HOME/.bashrc
-else
-  echo 'n1'
+  echo '.bash_profile not found. PWD == > $DOTFILES_ROOT.'
+  echo ' The "install.sh" has to be run from inside the "dotfiles" folder.'
 fi
