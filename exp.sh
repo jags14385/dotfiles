1312167ff34bb3d7dfe21fcdb1fba897a9f9cbf0
Jags <jagan@Jagans-MacBook-Pro.local>
2015-09-07 21:13:53 +1000

    Ensuring that files not added to profile, if already added.

--- exp.sh
+++ exp.sh
@@ -2,11 +2,22 @@
 
 USERNAME=$USER
 DOTFILES_ROOT=$(pwd)
-GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases.sh
-SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases.sh
+GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases
+SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases
 BASH_PROFILE_PATH='/Users/'$USERNAME'/.bash_profile'
 
 #Checking if the file exists and has execute permissions
 if [ -e $GIT_ALIASES_PATH -a ! -x $GIT_ALIASES_PATH ]; then
   chmod +x $GIT_ALIASES_PATH
+fi
+
+#Checking if the files are already added to the bash profile
+CMD_CAT='cat '$BASH_PROFILE_PATH' | grep -i "source '$GIT_ALIASES_PATH'" | wc -l'
+echo $CMD_CAT
+ad=`eval "$CMD_CAT"`
+echo $ad
+if [ $ad -eq 1 ]; then
+  echo "Y"
+else
+  echo "N"
 fi
--- install.sh
+++ install.sh
@@ -15,8 +15,16 @@ if [ -e $SYS_ALIASES_PATH -a ! -x $SYS_ALIASES_PATH ]; then
 fi
 
 if [ -e $BASH_PROFILE_PATH -a -e $GIT_ALIASES_PATH -a -e $SYS_ALIASES_PATH ]; then
-  echo 'source '$GIT_ALIASES_PATH >> $BASH_PROFILE_PATH
-  echo 'source '$SYS_ALIASES_PATH >> $BASH_PROFILE_PATH
+  CMD='cat '$BASH_PROFILE_PATH' | grep -i "source '$GIT_ALIASES_PATH'" | wc -l'
+  op=`eval "$CMD"`
+  if [ $op -eq 0 ];then
+    echo 'source '$GIT_ALIASES_PATH >> $BASH_PROFILE_PATH
+  fi
+  CMD='cat '$BASH_PROFILE_PATH' | grep -i "source '$SYS_ALIASES_PATH'" | wc -l'
+  op=`eval "$CMD"`
+  if [ $op -eq 0 ]; then
+    echo 'source '$SYS_ALIASES_PATH >> $BASH_PROFILE_PATH
+  fi
   source $BASH_PROFILE_PATH
 else
   echo 'One or more files not found. PWD => $DOTFILES_ROOT.'
