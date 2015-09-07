e9fd957837e67560e4a1f9db958756d96ead1113
Jags <jagan@Jagans-MacBook-Pro.local>
2015-09-07 21:31:19 +1000

    Moved repeatable code to a method.

--- exp.sh
+++ exp.sh
@@ -6,18 +6,22 @@ GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases
 SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases
 BASH_PROFILE_PATH='/Users/'$USERNAME'/.bash_profile'
 
+echo_var(){
+  CMD_CAT='cat '$BASH_PROFILE_PATH' | grep -i "source '$1'" | wc -l'
+  echo $CMD_CAT
+  ad=`eval "$CMD_CAT"`
+  echo $ad
+  if [ $ad -eq 1 ]; then
+    echo "Y"
+  else
+    echo "N"
+  fi
+}
+
 #Checking if the file exists and has execute permissions
 if [ -e $GIT_ALIASES_PATH -a ! -x $GIT_ALIASES_PATH ]; then
   chmod +x $GIT_ALIASES_PATH
 fi
 
 #Checking if the files are already added to the bash profile
-CMD_CAT='cat '$BASH_PROFILE_PATH' | grep -i "source '$GIT_ALIASES_PATH'" | wc -l'
-echo $CMD_CAT
-ad=`eval "$CMD_CAT"`
-echo $ad
-if [ $ad -eq 1 ]; then
-  echo "Y"
-else
-  echo "N"
-fi
+echo_var "$GIT_ALIASES_PATH"
--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+check_if_present_in_bash_profile(){
+  CMD='cat '$BASH_PROFILE_PATH' | grep -i "source '$1'" | wc -l'
+  op=`eval "$CMD"`
+  if [ $op -eq 0 ];then
+    echo 'source '$1 >> $BASH_PROFILE_PATH
+  fi
+}
+
 USERNAME=$USER
 DOTFILES_ROOT=$(pwd)
 GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases
@@ -15,17 +23,10 @@ if [ -e $SYS_ALIASES_PATH -a ! -x $SYS_ALIASES_PATH ]; then
 fi
 
 if [ -e $BASH_PROFILE_PATH -a -e $GIT_ALIASES_PATH -a -e $SYS_ALIASES_PATH ]; then
-  CMD='cat '$BASH_PROFILE_PATH' | grep -i "source '$GIT_ALIASES_PATH'" | wc -l'
-  op=`eval "$CMD"`
-  if [ $op -eq 0 ];then
-    echo 'source '$GIT_ALIASES_PATH >> $BASH_PROFILE_PATH
-  fi
-  CMD='cat '$BASH_PROFILE_PATH' | grep -i "source '$SYS_ALIASES_PATH'" | wc -l'
-  op=`eval "$CMD"`
-  if [ $op -eq 0 ]; then
-    echo 'source '$SYS_ALIASES_PATH >> $BASH_PROFILE_PATH
-  fi
+  check_if_present_in_bash_profile "$GIT_ALIASES_PATH"
+  check_if_present_in_bash_profile "$SYS_ALIASES_PATH"
   source $BASH_PROFILE_PATH
+  echo "Aiases added."
 else
   echo 'One or more files not found. PWD => $DOTFILES_ROOT.'
   echo ' The "install.sh" has to be run from inside the "dotfiles" folder.'
