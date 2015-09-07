75c41ae9faa7c6ee2be0d9d7b81491903cbb67e3
Jags <jagan@Jagans-MacBook-Pro.local>
2015-09-07 19:47:33 +1000

    Spike Code.

--- exp.sh
+++ exp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+USERNAME=$USER
 DOTFILES_ROOT=$(pwd)
-GIT_ALIASES=$DOTFILES_ROOT/git/aliases.sh
-SYS_ALIASES=$DOTFILES_ROOT/sys/aliases.sh
+GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases.sh
+SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases.sh
+BASH_PROFILE_PATH='/Users/'$USERNAME'/.bash_profile'
 
-
-echo $GIT_ALIASES
-echo $SYS_ALIASES
+#Checking if the file exists and has execute permissions
+if [ -e $GIT_ALIASES_PATH -a ! -x $GIT_ALIASES_PATH ]; then
+  chmod +x $GIT_ALIASES_PATH
+fi
