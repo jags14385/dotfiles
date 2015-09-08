7e3ae7a8deb97b48ef47e9c24c4d341980ba993a
unknown <jagannath.vaikuntham@AUMEL010204N.myob.myobcorp.net>
2015-09-08 13:54:05 +1000

    Checked whether the OS is windows or linux  before setting the .bash_profile path. Need to refactor.

--- install.sh
+++ install.sh
@@ -8,11 +8,17 @@ check_if_present_in_bash_profile(){
   fi
 }
 
-USERNAME=$USER
+USERNAME=$USERNAME
 DOTFILES_ROOT=$(pwd)
 GIT_ALIASES_PATH=$DOTFILES_ROOT/git/aliases
 SYS_ALIASES_PATH=$DOTFILES_ROOT/sys/aliases
 BASH_PROFILE_PATH='/Users/'$USERNAME'/.bash_profile'
+#set bash profile path depending on whether the system is wuindows or *nix .
+CMD='uname -a | grep -i "MINGW" | wc -l'
+op=`eval "$CMD"`
+if [ $op -eq 1 ]; then
+  BASH_PROFILE_PATH='/c/Users/'$USERNAME'/.bash_profile'
+fi
 
 if [ -e $GIT_ALIASES_PATH -a ! -x $GIT_ALIASES_PATH ]; then
   chmod +x $GIT_ALIASES_PATH
@@ -22,12 +28,19 @@ if [ -e $SYS_ALIASES_PATH -a ! -x $SYS_ALIASES_PATH ]; then
   chmod +x $SYS_ALIASES_PATH
 fi
 
+if [ ! -e $BASH_PROFILE_PATH ]; then
+  echo $BASH_PROFILE_PATH' Creating .bash_profile file at the path mentioned.'
+  touch $BASH_PROFILE_PATH
+fi
+
 if [ -e $BASH_PROFILE_PATH -a -e $GIT_ALIASES_PATH -a -e $SYS_ALIASES_PATH ]; then
   check_if_present_in_bash_profile "$GIT_ALIASES_PATH"
   check_if_present_in_bash_profile "$SYS_ALIASES_PATH"
-  source $BASH_PROFILE_PATH
   echo "Aiases added."
 else
-  echo 'One or more files not found. PWD => $DOTFILES_ROOT.'
+  echo 'One or more files not found. PWD => '$DOTFILES_ROOT'.'
+  echo 'GIT_ALIASES_PATH => '$GIT_ALIASES_PATH
+  echo 'SYS_ALIASES_PATH => '$SYS_ALIASES_PATH
   echo ' The "install.sh" has to be run from inside the "dotfiles" folder.'
 fi
+  . $BASH_PROFILE_PATH
